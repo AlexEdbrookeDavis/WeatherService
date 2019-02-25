9b08c2766c34fdb53b57df0796b8560fef75ffd2
zosit <zosit123@hotmail.com>
2019-02-25 13:52:39 -0800

    Slightly improved test code

--- riposte-microservice-template-core-code/src/test/scala/com/myorg/ripostemicroservicetemplate/endpoints/proxyEndpointTest.scala
+++ riposte-microservice-template-core-code/src/test/scala/com/myorg/ripostemicroservicetemplate/endpoints/proxyEndpointTest.scala
@@ -2,6 +2,7 @@ package com.myorg.ripostemicroservicetemplate.endpoints;
 import com.personal.weatherservice.endpoints.ProxyEndpoint;
 
 import com.nike.riposte.server.http.ResponseInfo;
+import com.nike.riposte.util.Matcher;
 
 import org.junit.Test;
 
@@ -9,14 +10,22 @@ import java.util.concurrent.CompletableFuture;
 
 import org.junit.Assert._
 
+
+
 class ProxyEndpointTest {
     private var proxyEndpoint: ProxyEndpoint  = _
 
+    def testConstruction {
+        proxyEndpoint = new ProxyEndpoint()
+
+        assertEquals(proxyEndpoint.requestMatcher(), Matcher.`match`("/exampleProxy"))
+    }
+
+    //NOTE: this test is already performed through the unit tests of riposte. Since ProxyEndpoint is just an scala based Adapter for a preexisting class, there is no code to test other then construction.
     def testResponse {
-        proxyEndpoint = new ProxyEndpoint 
         // when
         var responseFuture: CompletableFuture[ResponseInfo[Void]] = CompletableFuture.completedFuture(ResponseInfo.newBuilder[Void]().withHttpStatusCode(200).build());
-        val responseInfo: ResponseInfo[Void] = responseFuture.join();
+        val responseInfo: ResponseInfo[Void] = responseFuture.join()
         // then
         assertEquals(responseInfo.getHttpStatusCode(), 200)
     }
